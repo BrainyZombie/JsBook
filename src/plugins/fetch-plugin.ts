2b85853b573dd0173f858d08eb2b2cfabddad7af
ashir basu <ashirbasu@gmail.com>
2022-03-13 23:09:46 +0530

    seperated caching and loading to a different module

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,45 @@
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputCode,
+          };
+        }
+
+        const cacheKey = args.path;
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          cacheKey
+        );
+
+        if (cachedResult) {
+          console.log("Using cached");
+          return cachedResult;
+        } else {
+          console.log("Not Using cached");
+        }
+
+        const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(cacheKey, result);
+        return result;
+      });
+    },
+  };
+};
