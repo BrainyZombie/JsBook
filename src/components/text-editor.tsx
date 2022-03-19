66a673032426889b7f2b3892c8d7a43780ced784
ashir basu <ashirbasu@gmail.com>
2022-03-19 16:36:44 +0530

    Added basic MD Editor component

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,43 @@
+import MDEditor from "@uiw/react-md-editor";
+import React, { useState, useEffect, useRef } from "react";
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const Listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", Listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", Listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      onClick={(event) => {
+        setEditing(true);
+      }}
+    >
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
